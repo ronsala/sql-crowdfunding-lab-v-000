9752f98dd33a2b3856f36c886a18adc64c4a20fa
Ron Sala <ron.sala@gmail.com>
2018-05-14 04:05:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO projects VALUES (1, 'moon base', 'space', 10000000, "dark brown", "calm", 1);
+INSERT INTO projects VALUES (1, 'moon base', 'space', 10000000, "2018-05-31", "2018-06-30");
