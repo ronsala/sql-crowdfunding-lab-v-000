e93c7d70b95b3fc0d23b6b20940306a213f6a28d
Ron Sala <ron.sala@gmail.com>
2018-05-14 06:16:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,8 +6,11 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+# selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+def
+  "SELECT projects.title, pledges.amount
+  FROM projects, pledges
+  SORT BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
