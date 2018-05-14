70a8a5f973127578876758a724271f54e0828f1d
Ron Sala <ron.sala@gmail.com>
2018-05-14 06:26:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,8 +6,8 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-# selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-def
+
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT title
   FROM projects
   LEFT OUTER JOIN pledges(amount)
