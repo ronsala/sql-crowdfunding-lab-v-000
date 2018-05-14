f6e6385d9742e0dfed616196a57aa95bed1e4e83
Ron Sala <ron.sala@gmail.com>
2018-05-14 08:39:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
   FROM pledges
-  LEFT OUTER JOIN projects
+  JOIN projects
   ON pledges.project_id = projects.id
   WHERE projects.category = 'music';"
 end
