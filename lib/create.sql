57b461cacfc3eae538ff76af6dec5b4858143127
Ron Sala <ron.sala@gmail.com>
2018-05-14 08:46:32 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,18 +1,19 @@
-CREATE TABLE project (
+CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
+  title TEXT,
   category TEXT,
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
-CREATE TABLE pledge (
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
