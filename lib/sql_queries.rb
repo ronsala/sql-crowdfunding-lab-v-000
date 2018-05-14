49ae9839c8706639b1028f59c4218c2634c1319f
Ron Sala <ron.sala@gmail.com>
2018-05-14 07:47:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,10 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT projects.title, SUM(pledges.amount)
+  FROM projects LEFT OUTER JOIN pledges
+  ON projects.id = pledges.project_id
+  GROUP BY projects.title;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
