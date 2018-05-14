c7a404e87ec4c66e1d4efb53675a72e8dc8975be
Ron Sala <ron.sala@gmail.com>
2018-05-14 08:25:44 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, pledges.amount
   FROM pledges
   LEFT OUTER JOIN users
-  ON pledges.user_id = users.project_id
+  ON pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY pledges.amount;"
 end
