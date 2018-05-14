05e50e21d65fc67a158da24426f38ee7d08362fe
Ron Sala <ron.sala@gmail.com>
2018-05-14 07:16:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,8 +9,8 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title, pledges.amount
-  FROM (projects LEFT OUTER JOIN pledges
-  ON projects.id = pledges.project_id)
+  FROM projects LEFT OUTER JOIN pledges
+  ON projects.id = pledges.project_id
   ORDER BY projects.title ASC;"
 end
 
